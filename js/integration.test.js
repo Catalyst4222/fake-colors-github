edfe8a891e2ad4067bba981c9ae0b4b1d964aeb8
Catalyst4 <catalyst4222@gmail.com>
2026-04-27 13:00:49 -0400

    Use fetch api in tests

--- js/integration.test.js
+++ js/integration.test.js
@@ -1,21 +1,36 @@
 const { expect, test } = require("@jest/globals");
 
-test("Hashes some random strings", () => {
+test("Hashes some random strings", async () => {
   let tmp = { login: "unknown", password: "nonexistant" };
   let jsonPayload = JSON.stringify(tmp);
 
   let url = "http://localhost" + "/Login." + "php";
 
-  let xhr = new XMLHttpRequest();
-  xhr.open("POST", url, true);
-  xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
-  xhr.onreadystatechange = function () {
-    expect(JSON.parse(xhr.responseText)).toEqual({
-      id: 0,
-      firstName: "",
-      lastName: "",
-      error: "No Records Found",
-    });
-  };
-  xhr.send(jsonPayload);
+  //   let xhr = new XMLHttpRequest();
+  //   xhr.open("POST", url, true);
+  //   xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
+  //   xhr.onreadystatechange = function () {
+  //     expect(JSON.parse(xhr.responseText)).toEqual({
+  //       id: 0,
+  //       firstName: "",
+  //       lastName: "",
+  //       error: "No Records Found",
+  //     });
+  //   };
+  //   xhr.send(jsonPayload);
+
+  const response = await fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-type": "application/json; charset=UTF-8",
+    },
+    body: jsonPayload,
+  });
+
+  expect(await response.json()).toEqual({
+    id: 0,
+    firstName: "",
+    lastName: "",
+    error: "No Records Found",
+  });
 });
